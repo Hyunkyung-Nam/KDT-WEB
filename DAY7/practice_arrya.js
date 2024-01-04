8d7deb1ffea101b309c26489840009a219c516e8
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-01-04 22:56:40 +0900

    오늘의집 클론코딩 240104 버전 애니메이션 및 flex 조정

---
+++ DAY7/practice_arrya.js
@@ -0,0 +1,10 @@
+let rainbow = ["red", "black", "orange", "green", "blue", "navy" ,"purple"];
+
+console.log(rainbow[2]);
+
+rainbow.push("yellow");
+console.log(rainbow[7]);
+
+console.log(rainbow.indexOf("black"));
+
+console.log(rainbow.reverse());
