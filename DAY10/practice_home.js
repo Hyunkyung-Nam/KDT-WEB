6159b791c6ffd292e0ba5fb85e2448e526233cb7
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-01-09 09:13:59 +0900

    0108 공부

--- DAY10/practice_home.js
+++ DAY10/practice_home.js
@@ -1,5 +1,19 @@
-let str = 'abcde';
-let arrays = str.split('');
-for (let i = 0; i < arrays.length; i++) {
-    console.log(arrays[i]);
+let my_string = 'He11oWor1d';
+let overwrite_string = 'lloWorl';
+let s = 2;
+
+function solution(my_string, overwrite_string, s) {
+    let answer = my_string.split('');
+    console.log(answer);
+    let overwrite_arr = overwrite_string.split('');
+    console.log(overwrite_arr);
+    for (let i = 0; i < overwrite_arr.length; i++) {
+        console.log(answer[s + i], overwrite_arr[i]);
+        answer[s + i] = overwrite_arr[i];
+        console.log(answer[s + i]);
+    }
+    answer = answer.toString().replace(',', '');
+    return answer;
 }
+
+console.log(solution(my_string, overwrite_string, s));
