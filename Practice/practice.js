6c0155968542a781babc6f972c62c7f55c09a890
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-01-10 18:28:57 +0900

    실습에 버튼호버시 커서포인터 변경

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,5 +1,31 @@
-let box = document.getElementById('box1');
+let arr = [0, 1, 2, 4, 3];
+let queries = [
+    [0, 4, 2],
+    [0, 3, 2],
+    [0, 2, 2],
+];
+var answer = [];
+for (query of queries) {
+    let index1 = query[0];
+    let index2 = query[1];
+    let compare = query[2];
+    let solutionNum = compare;
+    let count = 0;
 
-console.log(box);
-box.remove();
-console.log(box);
+    for (let i = index1; i <= index2; i++) {
+        if (arr[i] > compare) {
+            if (count === 0 || arr[i] < solutionNum) {
+                solutionNum = arr[i];
+                console.log(solutionNum);
+            }
+            count++;
+        }
+    }
+    if (count === 0) {
+        answer.push(-1);
+    } else {
+        answer.push(solutionNum);
+    }
+    count = 0;
+}
+console.log(answer);
